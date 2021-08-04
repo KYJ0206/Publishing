1d9130457b16b3b229f6868685632645eec1fd6f
K.Y.J <87288376+KYJ0206@users.noreply.github.com>
2021-08-04 16:52:44 +0900

    NEW (08-04)

--- 006.파일럿PJ/002.구현소스/js/men.js
+++ 006.파일럿PJ/002.구현소스/js/men.js
@@ -2,4 +2,34 @@
 
 $(function(){ /// jQB ////////////////////////////
 
+    /// 부드러운 스크롤 호출!(제이쿼리 아님!)
+    startSS();
+
+    ////////////////////////////////////
+    // 배너에 스와이퍼 플러그인 적용하기 //
+    ////////////////////////////////////
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        speed: 500,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+            // disableOnInteraction - 건드린후 멈춤 true
+            // disableOnInteraction - 건드린후 작동 false
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    }); ///// swiper /////
+
+    // 스크롤 등장 플러그인 적용 : 스크롤리빌
+    $.fn.scrollReveal(); 
+        
 }); ///////////// jQB ////////////////////////////
